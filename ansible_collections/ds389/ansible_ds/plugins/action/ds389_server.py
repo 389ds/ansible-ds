4d3575ee638494b595ca50688b0df3dee14a8c3c
Pierre Rogier <progier@redhat.com>
2023-03-23 22:15:30 +0100

    Fix lint errors in ds389_entities.py and some bugs

--- ansible_collections/ds389/ansible_ds/plugins/action/ds389_server.py
+++ ansible_collections/ds389/ansible_ds/plugins/action/ds389_server.py
@@ -384,7 +384,6 @@ class _PH:
 
     def validate_topology(self):
         """Perform global topology consistency checks."""
-<<<<<<< HEAD
         rids = {} # { "rid::suffix" : bename }
         errors = []
         for bevname,bedict in self.replbes.items():
@@ -411,9 +410,6 @@ class _PH:
                 errors.append((f"Backend {bevname} is configured as a consumer but has replica agreements.",))
         if errors:
             raise AnsibleError(f'Topology errors: {errors}')
-=======
-        pass
->>>>>>> 8304674 (Issue 31 - Add ds389_info plugin)
 
 
 class ActionModule(ActionBase):
